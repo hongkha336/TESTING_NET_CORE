fa1a379f3031fd27a15dfb833f37830c2e34a2c1
hongkha336 <45200419+hongkha336@users.noreply.github.com>
2019-12-14 01:05:31 +0700

    upload project

---
+++ testing-netcore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testing_netcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
